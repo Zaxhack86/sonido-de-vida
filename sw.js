1fedf99c5a454f9cbc4c51c8aba4b713464500b5
Zax <zax@sonidodevida.com>
2026-06-27 00:12:46 -0400

    Eliminar gestor de tracks (rueda ⚙) del Modo Enfoque

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_STATIC = 'sdv-static-v135';
+const CACHE_STATIC = 'sdv-static-v136';
 const CACHE_AUDIO  = 'sdv-audio-v2';
 
 // La app (PWA) vive en la raíz ('/'). Ya no hay landing ni '/app'; '/app' y
